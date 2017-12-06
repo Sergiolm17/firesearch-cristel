29a876736d8f7ce48ba601006c1ae08cd4d39142
Sergiolm17 <kingsergiolm17@gmail.com>
2017-12-05 21:54:40 -0500

    update json

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const firebase = require('firebase');
 
 const express = require('express')
 const app = express()
-app.get('/',function(req,res){
+app.get('/', (req,res) => {
   res.sendFile(__dirname + '/index.html');
 });
 app.listen(process.env.PORT || 5000)
